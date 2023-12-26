61fc6b7506202342b559ce72e85ff965a88c1d5b
Bhargav200004 <Bhargav200004@gmail.com>
2023-12-26 23:04:16 +0530

    commit #6 (#3)

--- app/src/main/java/com/example/loginauth/data/LoginViewModel.kt
+++ app/src/main/java/com/example/loginauth/data/LoginViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.loginauth.data
 
+
+import android.content.ContentValues.TAG
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -49,15 +51,9 @@ class LoginViewModel : ViewModel() {
         printState()
     }
 
+
     private fun printState() {
         Log.d(TAG, "UIState")
-        Log.d(TAG, registrationUIState.value.toString())
-        }
+        Log.d(TAG, registrationUIState.value.toString ())
     }
-
-    private fun printState() {
-        Log.d("TAG", "UIState")
-        Log.d("TAG", registrationUIState.value.toString()
-    }
-
 }
